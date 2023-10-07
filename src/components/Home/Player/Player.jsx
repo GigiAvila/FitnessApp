fa28aa68e1f734eb8b1360f8141bc29da6d6e137
GigiAvila <gigi.s.avila@gmail.com>
2023-10-07 14:55:36 +0200

    changeReactPlayerDep

--- src/components/Home/Player/Player.jsx
+++ src/components/Home/Player/Player.jsx
@@ -1,60 +1,55 @@
-import React, { useState, useEffect, useContext } from 'react';
-import ReactPlayer from 'react-player';
-import { VideoPopover, Tooltip } from './Player.Styles';
-import ReactDOM from 'react-dom';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { useTheme } from '@emotion/react';
-import VideoButtons from './VideoButtons';
-import EndRoutineMessage from './EndRoutineMessage';
-import { RoutinesContext } from '../../../context/RoutinesContext';
+import React, { useState, useEffect, useContext } from 'react'
+import ReactPlayer from 'react-player'
+import { VideoPopover, Tooltip } from './Player.Styles'
+import ReactDOM from 'react-dom'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import { useTheme } from '@emotion/react'
+import VideoButtons from './VideoButtons'
+import EndRoutineMessage from './EndRoutineMessage'
+import { RoutinesContext } from '../../../context/RoutinesContext'
 
 function Player({ handleIsOpen, selectedRoutine }) {
-  const theme = useTheme();
-  const { addRoutine } = useContext(RoutinesContext);
+  const theme = useTheme()
+  const { addRoutine } = useContext(RoutinesContext)
 
-
-
-
-  const [showTooltip, setShowTooltip] = useState(false);
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-  const [showMessage, setShowMessage] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false)
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
+  const [showMessage, setShowMessage] = useState(false)
 
   const handleMouseEnter = () => {
-    setShowTooltip(true);
-  };
+    setShowTooltip(true)
+  }
 
   const handleMouseLeave = () => {
-    setShowTooltip(false);
-  };
+    setShowTooltip(false)
+  }
 
   const handleNextVideo = () => {
     if (currentVideoIndex < selectedRoutine.exercises.length - 1) {
-      setCurrentVideoIndex(currentVideoIndex + 1);
+      setCurrentVideoIndex(currentVideoIndex + 1)
     } else {
-      setShowMessage(true);
-      addRoutine(selectedRoutine);
+      setShowMessage(true)
+      addRoutine(selectedRoutine)
       console.log(selectedRoutine)
-
     }
-  };
-
+  }
 
   const handlePrevVideo = () => {
     if (currentVideoIndex === selectedRoutine.exercises.length - 1) {
-      setCurrentVideoIndex(currentVideoIndex - 1);
+      setCurrentVideoIndex(currentVideoIndex - 1)
     }
-  };
+  }
 
   useEffect(() => {
-    setCurrentVideoIndex(0);
-  }, [selectedRoutine]);
+    setCurrentVideoIndex(0)
+  }, [selectedRoutine])
 
   return (
     <>
       {ReactDOM.createPortal(
         <VideoPopover
           style={{
-            background: theme.palette.background,
+            background: theme.palette.background
           }}
         >
           <ArrowBackIosIcon
@@ -62,7 +57,7 @@ function Player({ handleIsOpen, selectedRoutine }) {
               color: theme.palette.color,
               position: 'fixed',
               top: '20px',
-              left: '20px',
+              left: '20px'
             }}
             onClick={handleIsOpen}
             onMouseEnter={handleMouseEnter}
@@ -72,7 +67,7 @@ function Player({ handleIsOpen, selectedRoutine }) {
             <Tooltip
               style={{
                 background: theme.palette.color,
-                color: theme.palette.background,
+                color: theme.palette.background
               }}
             >
               Go back
@@ -82,29 +77,23 @@ function Player({ handleIsOpen, selectedRoutine }) {
           <h5>{selectedRoutine.type}</h5>
           <ReactPlayer
             url={selectedRoutine.exercises[currentVideoIndex].video}
-            width="90%"
-            height="auto"
-            controls
-            config={{
-              file: {
-                attributes: {
-                  playsInline: true,
-                  preload: 'auto',
-                },
-              },
-            }}
+            width='90%'
+            height='auto'
+            controls={true}
+            playsInline={true}
             onEnded={handleNextVideo}
           />
-          <VideoButtons handleNextVideo={handleNextVideo} handlePrevVideo={handlePrevVideo} />
+          <VideoButtons
+            handleNextVideo={handleNextVideo}
+            handlePrevVideo={handlePrevVideo}
+          />
 
-          {showMessage ?
-            (<EndRoutineMessage />) : null
-          }
+          {showMessage ? <EndRoutineMessage /> : null}
         </VideoPopover>,
         document.getElementById('portal')
       )}
     </>
-  );
+  )
 }
 
-export default Player;
+export default Player
